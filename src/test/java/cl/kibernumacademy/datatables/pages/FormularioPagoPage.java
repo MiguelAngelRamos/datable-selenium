352146652b092ed64674bf353eb3c4ccfbb88d72
Miguel Ramos <return.infox@gmail.com>
2025-07-31 21:31:41 -0400

    codigo de banca medio y monto

--- src/test/java/cl/kibernumacademy/datatables/pages/FormularioPagoPage.java
+++ src/test/java/cl/kibernumacademy/datatables/pages/FormularioPagoPage.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FormularioPagoPage {
@@ -27,6 +29,21 @@ public class FormularioPagoPage {
   public void navigateTo() {
     driver.get("https://testing-qa.netlify.app/pages/banco.html");
   }
+
+  // Ingresar el monto
+  public void ingresarMonto(double monto) {
+    WebElement montoElement = wait.until(ExpectedConditions.elementToBeClickable(montoInput));
+    montoElement.clear();
+    montoElement.sendKeys(String.valueOf(monto));
+  }
+  // Selecionar el medio de pago
+  public void seleccionarMedioPago(String medio) {
+    if(medio != null && !medio.isEmpty()) {
+      WebElement medioElement = wait.until(ExpectedConditions.elementToBeClickable(medioSelect));
+      medioElement.sendKeys(medio);
+    }
+  }
+   // Obtener el resultado
 
 
   
