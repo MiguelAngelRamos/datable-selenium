aa1ff5e223d49424717b77b64bdec323af2e06de
Miguel Ramos <return.infox@gmail.com>
2025-07-31 20:54:31 -0400

    data tables

---
+++ src/test/java/cl/kibernumacademy/datatables/AppTest.java
@@ -0,0 +1,20 @@
+package cl.kibernumacademy.datatables;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
